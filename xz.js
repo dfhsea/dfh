c21fb7e2dc8b2f341dba58463e59798f5edc2376
dfhsea <123146788+dfhsea@users.noreply.github.com>
2024-07-01 05:27:47 +0800

    111

--- xz.js
+++ xz.js
@@ -1,5 +1,5 @@
 export async function fetch(request, env, ctx) {
   // 重新从 Durable Objects 中获取 xx 的值
-  const newValue = await env.cx.get('xx');
-  return newValue;
+  const 11newValue = await env.cx.get('xx');
+  return 11newValue;
 }
