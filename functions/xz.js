c1d24af7cab192055b0669a520989fec8cf42598
dfhsea <123146788+dfhsea@users.noreply.github.com>
2024-07-01 15:21:09 +0800

    Update xz.js

--- functions/xz.js
+++ functions/xz.js
@@ -1,24 +1,9 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
+export default {
+  async fetch(request, env, ctx) {
 
-async function handleRequest(request) {
-  // 假设 d1 的值通过环境变量获取
-  const d1Value = env.d1;
+    // 重新从 Durable Objects 中获取 xx 的值
+    const newValue = await env.cx.get('xx');
 
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>D1 值展示</title>
-    </head>
-    <body>
-      <h1>D1 的值为: ${d1Value}</h1>
-    </body>
-    </html>
-  `;
-
-  const headers = { 'Content-Type': 'text/html' };
-  return new Response(html, { headers });
-}
+    return new Response(JSON.stringify({ xx: newValue }), {headers: { 'Content-Type': 'application/json' }});
+  },
+};
